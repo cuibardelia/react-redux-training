6637d11fea01f16834169daefdea3b50dca817d4
deliac <delia.cuibar@synertrade.com>
2019-03-05 16:24:55 +0200

    Async Writes in Redux - the thunk chapter
    - Add mock APIs
    - Add CourseList and CourseListRow components and subsequently add these to the CoursesPage
    - update actions and reducer based on the new components
    - add thunk to actionTypes.js

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,10 +2,12 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state,
-      Object.assign({}, action.course)
-      ]; //not correct state.push(action.course) return state;
+    // case types.CREATE_COURSE:
+    //   return [...state,
+    //   Object.assign({}, action.course)
+    //   ];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
 
     default:
       return state;
