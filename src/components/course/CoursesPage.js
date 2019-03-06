6c92a9b8332c1507aaf2ba84b2c863048fc6d7f5
deliac <delia.cuibar@synertrade.com>
2019-03-06 18:08:09 +0200

    Savecourse

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from "react";
 import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
 import {bindActionCreators} from "redux";
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
@@ -7,10 +8,15 @@ import CourseList from './CourseList';
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind();
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
   }
 
   render() {
@@ -18,6 +24,10 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
+        <input type="submit"
+        value="Add Course"
+        className="bun btn-primary"
+        onClick={this.redirectToAddCoursePage} />
         <CourseList courses={courses} />
       </div>
     );
